6c6108214819478d61d2a6445c7dc6d54d53c4cb
julius caezar <julius.caezar25@gmail.com>
2025-12-12 15:36:16 -0300

    new component for link animation

---
+++ src/app/components/ui/link-arrow.tsx
@@ -0,0 +1,38 @@
+import { ArrowRight, ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+
+interface LinkArrowProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const LinkArrow = ({ children, href, className }: LinkArrowProps) => {
+  return (
+    <Link
+      href={href}
+      target={`${
+        href.startsWith("http") || href.startsWith("mailto") ? "_blank" : ""
+      }`}
+      className={`group flex items-center gap-1 transition-all duration-200 ease-in-out hover:text-neutral-500 ${
+        className ?? ""
+      }`}
+    >
+      {children}
+
+      {href.startsWith("http") || href.startsWith("mailto") ? (
+        <ArrowUpRight
+          className={`h-2.5 w-2.5 opacity-0 transition-all duration-200 ease-in-out group-hover:translate-x-0.5 group-hover:opacity-100`}
+          strokeWidth={2.6}
+        />
+      ) : (
+        <ArrowRight
+          className={`h-2.5 w-2.5 text-neutral-400 opacity-0 transition-all duration-200 ease-in-out group-hover:translate-x-0.5 group-hover:opacity-100`}
+          strokeWidth={2.6}
+        />
+      )}
+    </Link>
+  );
+};
+
+export default LinkArrow;
